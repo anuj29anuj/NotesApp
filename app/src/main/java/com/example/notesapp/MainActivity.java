b2f8383972890e32f0ca2c4be08d4b04fd53b81e
Avendo07 <namankaushikplt@gmail.com>
2019-10-08 17:34:14 +0530

    Successful titles

--- app/src/main/java/com/example/notesapp/MainActivity.java
+++ app/src/main/java/com/example/notesapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.notesapp;
 
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.Menu;
@@ -17,9 +19,11 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
@@ -30,6 +34,9 @@ public class MainActivity extends AppCompatActivity {
     DatabaseReference database;
     ListView listView;
     Title title;
+    int n;
+    TextView TV;
+    SharedPreferences sharedPreferences;
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -47,18 +54,17 @@ public class MainActivity extends AppCompatActivity {
         final EditText tv = new EditText(this);
         tv.setGravity(Gravity.CENTER);
         new AlertDialog.Builder(this)
-                .setMessage("Enter the tittle of note")
+                .setMessage("Enter the title of note")
                 .setView(tv)
                 .setPositiveButton("Create", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
                         String head = tv.getText().toString();
-                        arrayList.add(head);
                         title = new Title();
                         title.setTitle(head);
-                        database.child("Note No n").setValue(title);
-                        Toast.makeText(MainActivity.this, "Success", Toast.LENGTH_SHORT).show();
-
+                        database.child("Note No "+(n+1)).setValue(title);
+                        sharedPreferences.edit().putInt("Number", n+1).apply();
+                        Toast.makeText(MainActivity.this, head+" Saved", Toast.LENGTH_SHORT).show();
                     }
                 })
                 .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
@@ -79,30 +85,52 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         listView = findViewById(R.id.list);
-        database = FirebaseDatabase.getInstance().getReference().child("Member");
+        database = FirebaseDatabase.getInstance().getReference().child("Notes");
 
+        sharedPreferences = this.getSharedPreferences("myPreferences", Context.MODE_PRIVATE);
+        n = sharedPreferences.getInt("Number", 0);
 
-        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+        /*listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
                 arrayList.remove(i);
                 arrayAdapter.notifyDataSetChanged();
-//                listView.requestLayout();
+                //listView.requestLayout();                                                                 //Remove from Database
                 //adapterView.refreshDrawableState();
                 return true;
             }
-        });
+        });*/
 
         arrayList = new ArrayList<>();
-        arrayList.add("Negan");
-        arrayList.add("Rick Grimes");
-        arrayList.add("Daryl");
-
-
         arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, arrayList);
         listView.setAdapter(arrayAdapter);
+
+        DatabaseReference retrieve = FirebaseDatabase.getInstance().getReference().child("Notes");
+        retrieve.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                arrayList.clear();
+                for(int i=1; i<=n; i++){
+                    try {
+                        String data = dataSnapshot.child("Note No " + i).child("title").getValue().toString();
+                        arrayList.add(data);
+                    }
+                    catch (Exception E){
+
+                    }
+                }
+                arrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+        for(int i=0; i<=n; i++){
+
+        }
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -114,4 +142,6 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+
+
 }
